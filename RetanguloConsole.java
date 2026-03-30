fbca083987849faae2f7ea72da7c81b97428bbb7
Gustavo Munoz Dutra <gustavomunozdutra@gmail.com>
2026-03-29 23:09:58 -0300

    construcao do mapa e posicionamento das minas via leitura do txt feitos

--- RetanguloConsole.java
+++ RetanguloConsole.java
@@ -11,29 +11,40 @@ public class RetanguloConsole {
             int largura = scanner.nextInt();   // w
             int altura = scanner.nextInt();    // h
             int numeroMinas = scanner.nextInt(); // m
+            int c1;
+            int c2;
 
             System.out.println("Config: Largura=" + largura + " Altura=" + altura + " Minas=" + numeroMinas);
 
             char[][] mapaMinas = new char[altura][largura];
 
-            for (int i = 0; i < altura; i++) {
-                for (int j = 0; j < largura; j++) {
-                    if (i == 0 || i == altura - 1) {
-                        mapaMinas[i][j] = '-';
-                    } else if (j == 0 || j == largura - 1) {
-                        mapaMinas[i][j] = '|';
-                    } else {
-                        mapaMinas[i][j] = ' ';
-                    }
-                    System.out.print(mapaMinas[i][j]);
+           for (int i = 0; i < altura; i++) {
+            for (int j = 0; j < largura; j++) {
+                if (i == 0 || i == altura - 1) {
+                    mapaMinas[i][j] = '-';
+                } else if (j == 0 || j == largura - 1) {
+                    mapaMinas[i][j] = '|';
+                } else {
+                    mapaMinas[i][j] = ' ';
                 }
-                System.out.println(); 
             }
+        }
 
             while (scanner.hasNextInt()) {
-                int x = scanner.nextInt();
-                int y = scanner.nextInt();
-                System.out.print(mapaMinas[y][x] = '*');
+                c1 = scanner.nextInt(); // Coluna (X)
+                c2 = scanner.nextInt(); // Linha (Y)
+                
+                // Verifica se a coordenada está dentro dos limites da matriz
+                if (c2 < altura && c1 < largura) {
+                    mapaMinas[c2][c1] = '*'; 
+                }
+            }
+
+            for (int i = 0; i < altura; i++) {
+                for (int j = 0; j < largura; j++) {
+                    System.out.print(mapaMinas[i][j]);
+                }
+                System.out.println(); // Pula linha ao final de cada linha do mapa
             }
 
         } catch (FileNotFoundException e) {

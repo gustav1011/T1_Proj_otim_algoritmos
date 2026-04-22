6272a8309f2fb9494c6521fbadfd49ff14aba1db
Gustavo Munoz Dutra <gustavomunozdutra@gmail.com>
2026-04-21 22:39:48 -0300

    tentando arrumar o estouro de memoria que ocorre a partir do teste 08

--- RetanguloConsole.java
+++ RetanguloConsole.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class RetanguloConsole {
 
     public static void main(String[] args) {
-        String path = "caso01.txt";
+        String path = "caso08.txt";
 
         try (Scanner scanner = new Scanner(new File(path))) {
 
@@ -18,16 +18,10 @@ public class RetanguloConsole {
             System.out.println("Config: Largura=" + largura + " Altura=" + altura + " Minas=" + numeroMinas);
 
             char[][] mapaMinas = new char[altura][largura];
-
+            
             for (int i = 0; i < altura; i++) {
                 for (int j = 0; j < largura; j++) {
-                    if (i == 0 || i == altura - 1) {
-                        mapaMinas[i][j] = '-';
-                    } else if (j == 0 || j == largura - 1) {
-                        mapaMinas[i][j] = '|';
-                    } else {
-                        mapaMinas[i][j] = '.';
-                    }
+                            mapaMinas[i][j] = '.';
                 }
             }
 
@@ -41,14 +35,15 @@ public class RetanguloConsole {
             }
 
             // imprime mapa
+        if(altura <= 500 && largura <= 500){
             for (int i = 0; i < altura; i++) {
                 for (int j = 0; j < largura; j++) {
                     System.out.print(mapaMinas[i][j]);
                 }
                 System.out.println();
             }
+        }
 
-            // ✅ AGORA ESTÁ NO LUGAR CERTO
             int maiorArea = verificaArea(mapaMinas, largura);
             System.out.println("\nMaior área livre: " + maiorArea);
 
